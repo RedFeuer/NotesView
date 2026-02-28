c07fab5851957f50b79b1afcef841c30ad21d749
RedFeuer <danilabespyatov@gmail.com>
2026-02-28 12:02:19 +0300

    REFACTORING: moved MainActivity to presentation and created Data

--- app/src/main/java/com/example/noteslist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/noteslist/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.noteslist
+package com.example.noteslist.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.noteslist.R
 
 //тут будешь ваша активити
 class MainActivity : AppCompatActivity() {
