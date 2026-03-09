c340dd180d138a2712f1e497a55f862863cc37cf
RedFeuer <danilabespyatov@gmail.com>
2026-03-09 09:41:12 +0300

    ADD: spToPx methods

--- app/src/main/java/com/example/noteslist/presentation/Util.kt
+++ app/src/main/java/com/example/noteslist/presentation/Util.kt
@@ -1,6 +1,7 @@
 package com.example.noteslist.presentation
 
 import android.content.res.Resources
+import android.util.TypedValue
 
 /* вспомогательное extension-свойство для конвертации dp в пиксели для корректного отображения на разных устройствах */
 val Float.dpToPx: Float
@@ -8,3 +9,17 @@ val Float.dpToPx: Float
 
 val Int.dpToPx: Int
     get() = this * Resources.getSystem().displayMetrics.density.toInt()
+
+val Float.spToPx: Float
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_SP,
+        this,
+        Resources.getSystem().displayMetrics
+    )
+
+val Int.spToPx: Int
+    get() = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_SP,
+        this.toFloat(),
+        Resources.getSystem().displayMetrics
+    ).toInt()
