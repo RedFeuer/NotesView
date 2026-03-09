02799b85a16d529784e84830127a996e00c39f4d
RedFeuer <danilabespyatov@gmail.com>
2026-03-09 21:27:56 +0300

    ADD: fabric method to create ImportantNoteViewHolder

--- app/src/main/java/com/example/noteslist/presentation/notes/holders/ImportantNoteViewHolder.kt
+++ app/src/main/java/com/example/noteslist/presentation/notes/holders/ImportantNoteViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.noteslist.presentation.notes.holders
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.noteslist.domain.domainModel.Note
 import com.example.noteslist.presentation.view.NoteMapper
@@ -13,5 +14,13 @@ class ImportantNoteViewHolder(
     fun bind(note: Note) {
         val noteUi = noteMapper.mapDomainModelToUi(note)
         noteView.bind(noteUi)
+    }
+
+    companion object {
+        /* фабричный метод для создания экземпляра ViewHolder, который будет использоваться в адаптере */
+        fun create (parent: ViewGroup) : ImportantNoteViewHolder {
+            val noteView = NoteView(parent.context)
+            return ImportantNoteViewHolder(noteView)
+        }
     }
 }
