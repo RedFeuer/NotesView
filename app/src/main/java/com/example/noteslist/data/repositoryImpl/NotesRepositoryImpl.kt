fa2ddd8411a4b6894abe1623fca3c4f4c42d8f5c
RedFeuer <danilabespyatov@gmail.com>
2026-04-05 13:44:41 +0300

    IMPROVE: broad Repository's contract and make it Singleton

--- app/src/main/java/com/example/noteslist/data/repositoryImpl/NotesRepositoryImpl.kt
+++ app/src/main/java/com/example/noteslist/data/repositoryImpl/NotesRepositoryImpl.kt
@@ -5,30 +5,61 @@ import com.example.noteslist.domain.repository.NotesRepository
 
 class NotesRepositoryImpl : NotesRepository {
     /* TODO: потом сделать Flow<Note>, чтобы подписаться в ViewModel и реактивно отображать на экране */
-    override fun getNotes() : List<Note> {
-        return listOf(
-//            Note(
+    private val notes = mutableListOf<Note>(
+        //            Note(
 //                title = "Т-Академия",
 //                isImportant = true,
 //                description = "Выполнить задание 1",
 //            ),
-            Note(
-                title = "Резюме",
+        Note(
+            title = "Резюме",
 //                isImportant = true,
-                description = "Добавить новый проект с BLE в резюме"
-            ),
-            Note(
-                title = "НИР",
-                description = "Согласовать задачи на семестр с научным руководителем"
-            ),
-            Note(
-                title = "Безопасность операционных систем",
-                description = "Отмечать неточности в книге преподавателя и писать ему, чтобы он их исправлял"
-            ),
-            Note(
-                title = "Сервис",
-                description = "Оплатить подписку",
-            ),
-        )
+            description = "Добавить новый проект с BLE в резюме"
+        ),
+        Note(
+            title = "НИР",
+            description = "Согласовать задачи на семестр с научным руководителем"
+        ),
+        Note(
+            title = "Безопасность операционных систем",
+            description = "Отмечать неточности в книге преподавателя и писать ему, чтобы он их исправлял"
+        ),
+        Note(
+            title = "Сервис",
+            description = "Оплатить подписку",
+        ),
+    )
+
+    override fun getNotes(): List<Note> {
+        return notes.sortedByDescending { it.createdAtMillis }
+    }
+
+    override fun getNoteById(uiId: String): Note? {
+        return notes.find { it.uiId == uiId }
+    }
+
+    override fun addNote(note: Note) {
+        notes.add(note)
+    }
+
+    override fun updateNote(note: Note) {
+        val index = notes.indexOfFirst { it.uiId == note.uiId }
+        if (index != -1) {
+            notes[index] = note
+        }
+    }
+
+    override fun toggleViewed(uiId: String) {
+        val index = notes.indexOfFirst { it.uiId == uiId }
+        if (index != -1) {
+            val oldNote = notes[index]
+            notes[index] = oldNote.copy(isViewed = !oldNote.isViewed)
+        }
+    }
+
+    /* Singleton экземпляр, чтобы все изменения проихсодили в нем */
+    /* TODO: потом это можно сделать просто в DI и убрать отсюда */
+    companion object {
+        val instance: NotesRepositoryImpl by lazy { NotesRepositoryImpl() }
     }
 }
