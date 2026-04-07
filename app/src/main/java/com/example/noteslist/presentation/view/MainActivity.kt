bc1eaafc15f83e4c28f93983680dd215e9316b60
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-04-07 23:37:33 +0300

    IMPROVE: delete useless imports

--- app/src/main/java/com/example/noteslist/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/noteslist/presentation/view/MainActivity.kt
@@ -1,20 +1,9 @@
 package com.example.noteslist.presentation.view
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.noteslist.R
-import com.example.noteslist.data.repositoryImpl.NotesRepositoryImpl
-import com.example.noteslist.presentation.editor.NoteEditorActivity
-import com.example.noteslist.presentation.notes.adapters.NotesListAdapter
-import com.example.noteslist.presentation.notes.toNoteListItems
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 //тут будешь ваша активити
 class MainActivity : AppCompatActivity() {
