03d21cf36775a51465e114153ceceb64fa53839f
Danila Bespyatov <bespiatov.ds@dozen.mephi.ru>
2026-04-09 00:31:42 +0300

    ADD: exit-check

--- app/src/main/java/com/example/noteslist/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/noteslist/presentation/view/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.noteslist.presentation.view
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.example.noteslist.R
+import com.example.noteslist.presentation.editor.NoteEditorFragment
 
 //тут будешь ваша активити
 class MainActivity : AppCompatActivity() {
@@ -12,5 +17,72 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    when {
+                        isTwoPane() && isDetailEditorOpened() -> {
+                            closeDetailEditor()
+                        }
+                        !isTwoPane() && isEditorOpenedNavHost() -> {
+                            popEditorFromNavHost()
+                        }
+                        else -> {
+                            showExitConfirmationDialog()
+                        }
+                    }
+                }
+            }
+        )
+    }
+
+    /** проверяем, есть ли в текущем layout правый контейнер detail_fragment_container
+     * по сути проверка, что мы в ландшафтном режиме ориентации*/
+    private fun isTwoPane() : Boolean {
+        return findViewById<View?>(R.id.detail_fragment_container) != null
+    }
+
+    private fun isDetailEditorOpened() : Boolean {
+        val fragment = supportFragmentManager.findFragmentById(R.id.detail_fragment_container)
+        return fragment is NoteEditorFragment
+    }
+
+    private fun closeDetailEditor() {
+        val fragment = supportFragmentManager.findFragmentById(R.id.detail_fragment_container)
+            ?: return
+
+        supportFragmentManager.beginTransaction()
+            .remove(fragment)
+            .commit()
+    }
+
+    private fun isEditorOpenedNavHost() : Boolean {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as? NavHostFragment
+                ?: return false
+
+        val currentDestinationId = navHost.navController.currentDestination?.id
+        return currentDestinationId == R.id.note_editor_fragment
+    }
+
+    private fun popEditorFromNavHost() {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as? NavHostFragment
+                ?: return
+
+        navHost.navController.popBackStack()
+    }
+
+    private fun showExitConfirmationDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Подтверждение выхода")
+            .setMessage("Вы точно хотите выйти?")
+            .setNegativeButton("Нет", null)
+            .setPositiveButton("Да") { _, _ ->
+                finish()
+            }
+            .show()
     }
 }
