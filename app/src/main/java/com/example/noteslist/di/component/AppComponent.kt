24677aa57ef00b83bda3fd836e8e55810568da54
RedFeuer <danilabespyatov@gmail.com>
2026-04-23 15:07:59 +0300

    ADD: AppComponent and MainActivityComponent

--- app/src/main/java/com/example/noteslist/di/component/AppComponent.kt
+++ app/src/main/java/com/example/noteslist/di/component/AppComponent.kt
@@ -1,4 +1,29 @@
 package com.example.noteslist.di.component
 
+import android.app.Application
+import com.example.noteslist.di.module.RepositoryModule
+import com.example.noteslist.di.module.SubcomponentsModule
+import com.example.noteslist.di.module.ViewModelModule
+import com.example.noteslist.di.scope.AppScope
+import com.example.noteslist.di.subcomponent.MainActivityComponent
+import dagger.BindsInstance
+import dagger.Component
+
+@AppScope
+@Component(
+    modules = [
+        RepositoryModule::class,
+        ViewModelModule::class,
+        SubcomponentsModule::class,
+    ]
+)
 interface AppComponent {
+    fun mainActivityComponentFactory() : MainActivityComponent.Factory
+
+    @Component.Factory
+    interface Factory{
+        fun create(
+            @BindsInstance application: Application,
+        ) : AppComponent
+    }
 }
---
+++ app/src/main/java/com/example/noteslist/di/subcomponent/MainActivityComponent.kt
@@ -0,0 +1,18 @@
+package com.example.noteslist.di.subcomponent
+
+import com.example.noteslist.di.scope.ActivityScope
+import com.example.noteslist.presentation.view.MainActivity
+import dagger.Subcomponent
+
+@ActivityScope
+@Subcomponent
+interface MainActivityComponent {
+    fun inject(activity: MainActivity)
+
+    fun notesListFragmentComponentFactory() : NotesListFragmentComponent
+
+    @Subcomponent.Factory
+    interface Factory {
+        fun create() : MainActivityComponent
+    }
+}
