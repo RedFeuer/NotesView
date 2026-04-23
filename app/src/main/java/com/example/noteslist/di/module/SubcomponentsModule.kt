2877845f5a9056035bdcf40d19f188a004e0815f
RedFeuer <danilabespyatov@gmail.com>
2026-04-23 15:13:10 +0300

    IMPROVE: add comment

--- app/src/main/java/com/example/noteslist/di/module/SubcomponentsModule.kt
+++ app/src/main/java/com/example/noteslist/di/module/SubcomponentsModule.kt
@@ -4,6 +4,7 @@ import com.example.noteslist.di.subcomponent.MainActivityComponent
 import com.example.noteslist.di.subcomponent.NotesListFragmentComponent
 import dagger.Module
 
+/* вынесены в сабкомпонент, так как отличается жизненный цикл */
 @Module(
     subcomponents = [
         MainActivityComponent::class,
