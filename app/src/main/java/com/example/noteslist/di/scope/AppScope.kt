890b3db826126f3b4ec95725219a978cb5d96b78
RedFeuer <danilabespyatov@gmail.com>
2026-04-26 18:26:29 +0300

    IMPROVE: add docs

--- app/src/main/java/com/example/noteslist/di/scope/AppScope.kt
+++ app/src/main/java/com/example/noteslist/di/scope/AppScope.kt
@@ -2,6 +2,7 @@ package com.example.noteslist.di.scope
 
 import javax.inject.Scope
 
+/** Scope-аннотация, отвечающая за жизненный цикл объекта */
 @Scope
 @Retention(AnnotationRetention.RUNTIME)
 annotation class AppScope
