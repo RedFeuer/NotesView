f34c125d194f7c813b1eaf2475a5bf71bc824acf
RedFeuer <danilabespyatov@gmail.com>
2026-04-26 19:14:21 +0300

    ADD: make functions suspend to launch it in ApplicationScope in UseCases. Also make Notes Flow to react to changes in UI

--- app/src/main/java/com/example/noteslist/data/repositoryImpl/NotesRepositoryImpl.kt
+++ app/src/main/java/com/example/noteslist/data/repositoryImpl/NotesRepositoryImpl.kt
@@ -1,60 +1,101 @@
 package com.example.noteslist.data.repositoryImpl
 
+import androidx.compose.runtime.mutableStateOf
+import com.example.noteslist.di.coroutine.qualifier.IoDispatcher
 import com.example.noteslist.domain.domainModel.Note
 import com.example.noteslist.domain.repository.NotesRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class NotesRepositoryImpl @Inject constructor() : NotesRepository {
-    /* TODO: потом сделать Flow<Note>, чтобы подписаться в ViewModel и реактивно отображать на экране */
-    private val notes = mutableListOf<Note>(
-        //            Note(
-//                title = "Т-Академия",
+class NotesRepositoryImpl @Inject constructor(
+    @IoDispatcher private val ioDispatcher : CoroutineDispatcher,
+) : NotesRepository {
+    private val notesFlow = MutableStateFlow(
+        listOf(
+            Note(
+                title = "Т-Академия",
+                isImportant = true,
+                description = "Выполнить задание 3",
+            ),
+            Note(
+                title = "Работа",
+                isImportant = true,
+                description = "Пройти испытательный срок"
+            ),
+            Note(
+                title = "Резюме",
 //                isImportant = true,
-//                description = "Выполнить задание 1",
-//            ),
-        Note(
-            title = "Резюме",
-//                isImportant = true,
-            description = "Добавить новый проект с BLE в резюме"
-        ),
-        Note(
-            title = "НИР",
-            description = "Согласовать задачи на семестр с научным руководителем"
-        ),
-        Note(
-            title = "Безопасность операционных систем",
-            description = "Отмечать неточности в книге преподавателя и писать ему, чтобы он их исправлял"
-        ),
-        Note(
-            title = "Сервис",
-            description = "Оплатить подписку",
-        ),
+                description = "Добавить новый проект с BLE в резюме"
+            ),
+            Note(
+                title = "НИР",
+                description = "Согласовать задачи на семестр с научным руководителем"
+            ),
+            Note(
+                title = "Безопасность операционных систем",
+                description = "Отмечать неточности в книге преподавателя и писать ему, чтобы он их исправлял"
+            ),
+            Note(
+                title = "Сервис",
+                description = "Оплатить подписку",
+            ),
+        )
     )
 
-    override fun getNotes(): List<Note> {
-        return notes.sortedByDescending { it.createdAtMillis }
+    override fun getNotes(): Flow<List<Note>> {
+        return notesFlow
+            .map { notes ->
+                notes.sortedByDescending { it.createdAtMillis }
+            }
+            .distinctUntilChanged()
     }
 
-    override fun getNoteById(uiId: String): Note? {
-        return notes.find { it.uiId == uiId }
-    }
-
-    override fun addNote(note: Note) {
-        notes.add(note)
-    }
-
-    override fun updateNote(note: Note) {
-        val index = notes.indexOfFirst { it.uiId == note.uiId }
-        if (index != -1) {
-            notes[index] = note
+    override suspend fun getNoteById(uiId: String): Note? {
+        return withContext(ioDispatcher) {
+            notesFlow.value.find { note -> note.uiId == uiId }
         }
     }
 
-    override fun toggleViewed(uiId: String) {
-        val index = notes.indexOfFirst { it.uiId == uiId }
-        if (index != -1) {
-            val oldNote = notes[index]
-            notes[index] = oldNote.copy(isViewed = !oldNote.isViewed)
+    override suspend fun addNote(note: Note) {
+        withContext(ioDispatcher) {
+            notesFlow.update { oldNotes ->
+                oldNotes + note
+            }
         }
+    }
+
+    override suspend fun updateNote(note: Note) {
+        withContext(ioDispatcher) {
+            notesFlow.update { oldNotes ->
+                oldNotes.map { oldNote ->
+                    if (oldNote.uiId == note.uiId) {
+                        note
+                    } else {
+                        oldNote
+                    }
+                }
+            }
+        }
+    }
+
+    override suspend fun toggleViewed(uiId: String) {
+       withContext(ioDispatcher) {
+           notesFlow.update { oldNotes ->
+               oldNotes.map { oldNote ->
+                   if (oldNote.uiId == uiId) {
+                       oldNote.copy(isViewed = !oldNote.isViewed)
+                   } else {
+                       oldNote
+                   }
+               }
+           }
+       }
     }
 }
--- app/src/main/java/com/example/noteslist/domain/repository/NotesRepository.kt
+++ app/src/main/java/com/example/noteslist/domain/repository/NotesRepository.kt
@@ -1,12 +1,12 @@
 package com.example.noteslist.domain.repository
 
 import com.example.noteslist.domain.domainModel.Note
+import kotlinx.coroutines.flow.Flow
 
-/* TODO: связать с реализацией */
 interface NotesRepository {
-    fun getNotes() : List<Note>
-    fun getNoteById(uiId : String): Note?
-    fun addNote(note : Note)
-    fun updateNote(note : Note)
-    fun toggleViewed(uiId : String) // помечаем прочитанным посредством длинного тапа
+    fun getNotes() : Flow<List<Note>>
+    suspend fun getNoteById(uiId : String): Note?
+    suspend fun addNote(note : Note)
+    suspend fun updateNote(note : Note)
+    suspend fun toggleViewed(uiId : String) // помечаем прочитанным посредством длинного тапа
 }
