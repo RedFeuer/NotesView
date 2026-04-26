dacbb898a6d667617cf9efd9ba79ff5d330ad33c
RedFeuer <danilabespyatov@gmail.com>
2026-04-26 13:32:27 +0300

    FIX: remove NotesListFragmentComponent as I already inject it in MainActivityComponent by notesListFragmentComponentFactory() method

--- app/src/main/java/com/example/noteslist/di/module/SubcomponentsModule.kt
+++ app/src/main/java/com/example/noteslist/di/module/SubcomponentsModule.kt
@@ -1,14 +1,12 @@
 package com.example.noteslist.di.module
 
 import com.example.noteslist.di.subcomponent.MainActivityComponent
-import com.example.noteslist.di.subcomponent.NotesListFragmentComponent
 import dagger.Module
 
 /* вынесены в сабкомпонент, так как отличается жизненный цикл */
 @Module(
     subcomponents = [
         MainActivityComponent::class,
-        NotesListFragmentComponent::class,
     ]
 )
 object SubcomponentsModule
