f5171f6a92f97c1278947e6b98d214847c1f5ed6
RedFeuer <danilabespyatov@gmail.com>
2026-04-26 18:26:11 +0300

    ADD: qualifier and IoDispatcher via Dagger

---
+++ app/src/main/java/com/example/noteslist/di/coroutine/qualifier/ApplicationCoroutineQualifier.kt
@@ -0,0 +1,8 @@
+package com.example.noteslist.di.coroutine.qualifier
+
+import javax.inject.Qualifier
+
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationCoroutineQualifier
