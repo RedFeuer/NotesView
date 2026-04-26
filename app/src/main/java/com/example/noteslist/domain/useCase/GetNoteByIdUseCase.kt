29bff8f7e71f653dded92b17a733908e80bec38f
RedFeuer <danilabespyatov@gmail.com>
2026-04-26 19:53:02 +0300

    ADD: UseCases for NotesRepository to use it in ViewModel

---
+++ app/src/main/java/com/example/noteslist/domain/useCase/GetNoteByIdUseCase.kt
@@ -0,0 +1,19 @@
+package com.example.noteslist.domain.useCase
+
+import com.example.noteslist.di.coroutine.qualifier.ApplicationCoroutineScope
+import com.example.noteslist.domain.repository.NotesRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class GetNoteByIdUseCase @Inject constructor(
+    private val notesRepository: NotesRepository,
+    @ApplicationCoroutineScope private val applicationCoroutineScope: CoroutineScope,
+) {
+    operator fun invoke(noteUiId: String) : Job {
+        return applicationCoroutineScope.launch {
+            notesRepository.getNoteById(noteUiId)
+        }
+    }
+}
