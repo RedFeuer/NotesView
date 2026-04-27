9ea9ebbca83553e5f88ce461eb7fd9a03dc3f64b
RedFeuer <danilabespyatov@gmail.com>
2026-04-27 15:44:37 +0300

    ADD: observeTitleLength() to look after title's length

--- app/src/main/java/com/example/noteslist/presentation/viewModel/NoteEditorViewModel.kt
+++ app/src/main/java/com/example/noteslist/presentation/viewModel/NoteEditorViewModel.kt
@@ -2,16 +2,24 @@ package com.example.noteslist.presentation.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.noteslist.di.coroutine.qualifier.DefaultDispatcher
 import com.example.noteslist.domain.domainModel.Note
 import com.example.noteslist.domain.useCase.CreateNoteUseCase
 import com.example.noteslist.domain.useCase.GetNoteByIdUseCase
 import com.example.noteslist.domain.useCase.UpdateNoteUseCase
 import com.example.noteslist.presentation.state.NoteEditorUiState
 import com.example.noteslist.presentation.view.NoteMapper
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.util.Date
 import javax.inject.Inject
@@ -21,16 +29,45 @@ class NoteEditorViewModel @Inject constructor(
     private val createNoteUseCase: CreateNoteUseCase,
     private val getNoteByIdUseCase: GetNoteByIdUseCase,
     private val noteMapper : NoteMapper,
+    @DefaultDispatcher private val defaultCoroutineDispatcher: CoroutineDispatcher,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(NoteEditorUiState())
     val uiState : StateFlow<NoteEditorUiState> = _uiState.asStateFlow()
 
     /** исходная заметка для режима редактирования */
     private var sourceNote : Note? = null
+    /** последний введенный заголовок */
+    private val titleChanges = MutableStateFlow<String>("")
+    companion object {
+        /** максимальная длина заметки */
+        const val MAX_TITLE_LENGTH = 50
+    }
     /** Job операции создания заметки */
     private var creationJob : Job? = null
     /** Job операции редактирования заметки */
     private var editionJob : Job? = null
+
+    init {
+        observeTitleLength()
+    }
+
+    /** следим за длиной названия заметки */
+    private fun observeTitleLength() {
+        titleChanges
+            .map { title ->
+                title.length > MAX_TITLE_LENGTH
+            } // преобразовываем к isTooLong
+            .distinctUntilChanged()
+            .flowOn(defaultCoroutineDispatcher) // не main-поток выше
+            .onEach { isTooLong ->
+                _uiState.update { current ->
+                    current.copy(
+                        showTitleTooLongError = isTooLong
+                    )
+                }
+            }
+            .launchIn(viewModelScope) // main-поток
+    }
 
     fun startCreate() {
         sourceNote = null
