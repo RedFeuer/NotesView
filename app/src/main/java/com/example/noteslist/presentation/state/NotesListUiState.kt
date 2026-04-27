a78c56a13d411a11addad48c8b6b5f5bcf1fa2b7
RedFeuer <danilabespyatov@gmail.com>
2026-04-27 21:26:52 +0300

    ADD: searchQuery in NotesListUiState

--- app/src/main/java/com/example/noteslist/presentation/state/NotesListUiState.kt
+++ app/src/main/java/com/example/noteslist/presentation/state/NotesListUiState.kt
@@ -5,4 +5,5 @@ import com.example.noteslist.presentation.notes.NoteListItem
 data class NotesListUiState(
     val items : List<NoteListItem> = emptyList(),
     val expandedStackIds : Set<String> = emptySet(),
+    val searchQuery : String = "",
 )
