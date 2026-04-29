00d2cae2b1e6a16cdad55922cff8503cb5b7197a
RedFeuer <danilabespyatov@gmail.com>
2026-04-29 15:46:03 +0300

    ADD: DemoNotesProvider

---
+++ app/src/main/java/com/example/noteslist/data/provider/DemoNotesProvider.kt
@@ -0,0 +1,41 @@
+package com.example.noteslist.data.provider
+
+import com.example.noteslist.domain.domainModel.Note
+import javax.inject.Inject
+
+class DemoNotesProvider @Inject constructor() {
+    fun create() : List<Note> {
+        return listOf(
+            Note(
+                title = "Т-Академия",
+                isImportant = true,
+                description = "Выполнить задание 3",
+            ),
+            Note(
+                title = "Работа",
+                isImportant = true,
+                description = "Пройти испытательный срок"
+            ),
+            Note(
+                title = "Безопасность операционных систем",
+                description = "Отмечать неточности в книге преподавателя и писать ему, чтобы он их исправлял"
+            ),
+            Note(
+                title = "Сервис",
+                description = "Оплатить подписку",
+            ),
+            /* прошлые сутки, чтобы проверить еще один стек */
+            Note(
+                title = "Резюме",
+//                isImportant = true,
+                description = "Добавить новый проект с BLE в резюме",
+                createdAtMillis = System.currentTimeMillis() - (24*60*60*1000)
+            ),
+            Note(
+                title = "НИР",
+                description = "Согласовать задачи на семестр с научным руководителем",
+                createdAtMillis = System.currentTimeMillis() - (24*60*60*1000)
+            )
+        )
+    }
+}
